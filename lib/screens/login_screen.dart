84f7773b982d15c651686d351136fa184b4c0935
Imronkhatib <imronskhatib@gmail.com>
2020-04-10 11:17:04 -0500

    Created login_screen layout and button

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email;
+  String _password;
+  _submit() {
+    if (_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram',
+              style: TextStyle(
+                fontFamily: 'Satisfy',
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'email'),
+                      validator: (input) => (input.contains('@')
+                          ? null
+                          : 'Please enter a valid email address'),
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'password'),
+                      validator: (input) => input.length < 6
+                          ? 'please enter a longer password' : null,
+                      onSaved: (input) => _password = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  FlatButton(
+                    onPressed: _submit,
+                    color: Colors.blue,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
